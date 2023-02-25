01e3579576a37eaa32935d91b9149d5425531326
reconvertido2022 <sododes@gmail.com>
2023-02-25 11:17:58 -0300

    Add files via upload

---
+++ trabajo-practico.js
@@ -0,0 +1,20 @@
+
+
+//comentario de una sola línea
+
+/**
+ * Comentario de 
+ * varias líneas
+ */
+  
+document.getElementById('boton').onclick = function (){
+    console.log ("Ejecuto el click");
+document.getElementById("Mail").innerHTML = "jorluduena@gmail.com";
+
+}
+
+document.getElementById('boton_fondo').onclick = function() {
+    
+document.body.style.backgroundColor = 'limegreen';
+};
+
